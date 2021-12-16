11dd975dcc83646455ab811187feae1fa262f4da
MrGri00 <marcgarciat2000@gmail.com>
2021-12-16 08:19:57 +0100

    Re-sync

---
+++ Project_Deepfall/Assets/Scripts/StateMachine/Player/State.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StateMachine
+{
+    [CreateAssetMenu(menuName = "StateMachine/State")]
+    public class State : ScriptableObject
+    {
+        public Action[] actions; // en un state se ejecutan varias acciones
+
+        public Transition[] transitions; // Desde un estado se puede pasar a otros estados a través de las transiciones
+
+        public void UpdateState(PlayerController controller) // Se ejecutan desde el Controller
+        {
+            DoActions(controller); // Ejecutamos todas las acciones
+            CheckTransitions(controller); // Comprobamos las transiciones
+        }
+
+        private void DoActions(PlayerController controller) // Ejecuta las acciones
+        {
+            for(int i = 0; i < actions.Length; i++)
+            {
+                actions[i].Act(controller); // Llamada al método abstracto
+            }
+        }
+
+        private bool AllDecisionsTrue(Transition currentTrans)
+        {
+            for (int i = 0; i < currentTrans.decisions.Length; i++)
+            {
+                if (currentTrans.decisions[i] == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void CheckTransitions(PlayerController controller)
+        {
+            for(int i = 0; i < transitions.Length; i++)
+            {
+                if(AllDecisionsTrue(transitions[i]))
+                {
+                    controller.Transition(transitions[i].trueState);
+                    return;
+                }
+                else
+                {
+                    controller.Transition(transitions[i].falseState);
+                }
+            }
+        }
+    }
+}
