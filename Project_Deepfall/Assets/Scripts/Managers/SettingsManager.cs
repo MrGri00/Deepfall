0e15d0ee25c8fc180703ba158a99da48c4ddd92d
MrGri00 <marcgarciat2000@gmail.com>
2022-01-24 21:44:33 +0100

    Volume slider is now functional

--- Project_Deepfall/Assets/Scripts/Managers/SettingsManager.cs
+++ Project_Deepfall/Assets/Scripts/Managers/SettingsManager.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioMixer audioMixer;
 
-    // Update is called once per frame
-    void Update()
+    public void SetVolume(float vol)
     {
-        
+        audioMixer.SetFloat("MasterVolume", vol);
     }
 }
