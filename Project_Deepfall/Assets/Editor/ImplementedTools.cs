04fc8d1621d4454d28bfb32469c2861c3f4524b2
MrGri00 <marcgarciat2000@gmail.com>
2022-01-25 09:29:47 +0100

    Implemented automation tools

---
+++ Project_Deepfall/Assets/Editor/ImplementedTools.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class ImplementedTools : MonoBehaviour
+{
+    [MenuItem("ImplementedTools/Toggle Active", false, 1)]
+    public static void ToggleActive()
+    {
+        Transform[] selectedElements = Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.Editable);
+
+        if (selectedElements.Length > 0)
+        {
+            for (int i = 0; i < selectedElements.Length; i++)
+            {
+                selectedElements[i].gameObject.SetActive(!selectedElements[i].gameObject.activeSelf);
+            }
+        }
+        else
+            EditorUtility.DisplayDialog("Error", "You must select at least one (1) element in the scene", "Ok");
+    }
+
+    [MenuItem("ImplementedTools/Sprites Auto Name Format", false, 1)]
+    public static void SpritesAutoNameFormat()
+    {
+        string[] sprites = AssetDatabase.FindAssets("t:sprite");
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            AssetDatabase.RenameAsset(AssetDatabase.GUIDToAssetPath(sprites[i]), RenameNoUnderscore(sprites[i]));
+        }
+
+        AssetDatabase.SaveAssets();
+    }
+
+    static string RenameNoUnderscore(string name)
+    {
+        string final = "";
+
+        if (name.Length > 1)
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (!name[i].Equals("_"))
+                    final += name[i];
+            }
+
+            return final;
+        }
+        else
+            return name;
+    }
+}
