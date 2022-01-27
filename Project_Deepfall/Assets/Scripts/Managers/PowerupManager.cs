85623467df374f28659d339612db4cb10ad62a68
MrGri00 <marcgarciat2000@gmail.com>
2022-01-27 09:52:27 +0100

    Powerups can now spawn across the level

--- Project_Deepfall/Assets/Scripts/Managers/PowerupManager.cs
+++ Project_Deepfall/Assets/Scripts/Managers/PowerupManager.cs
@@ -4,5 +4,60 @@ using UnityEngine;
 
 public class PowerupManager : MonoBehaviour
 {
-    
+    public float chanceMargin = 20f;
+
+    GameObject powerup = null;
+
+    private int powerupSpawnCoordinateY = -3;
+    private float powerupSpawnCoordinateX = 0f;
+    private float chance = 0f;
+    private int type = 0;
+
+    private void OnEnable()
+    {
+        MapManager.MapSpawned += PowerupSpawn;
+    }
+
+    private void OnDisable()
+    {
+        MapManager.MapSpawned -= PowerupSpawn;
+    }
+
+    void PowerupSpawn()
+    {
+        chance = UnityEngine.Random.Range(1f, 100f);
+
+        if (chance <= chanceMargin)
+        {
+            type = UnityEngine.Random.Range(0, 4);
+
+            powerupSpawnCoordinateX = UnityEngine.Random.Range(-4.5f, 4.5f);
+
+            switch (type)
+            {
+                case 0:
+                    powerup = PoolingManager.Instance.GetPooledObject("GunPowerup");
+                    break;
+
+                case 1:
+                    powerup = PoolingManager.Instance.GetPooledObject("ShotgunPowerup");
+                    break;
+
+                case 2:
+                    powerup = PoolingManager.Instance.GetPooledObject("LaserPowerup");
+                    break;
+
+                case 3:
+                    powerup = PoolingManager.Instance.GetPooledObject("HealthPowerup");
+                    break;
+            }
+
+            powerup.GetComponent<HealthManager>().ResetHealth();
+            powerup.transform.position = new Vector3(powerupSpawnCoordinateX, powerupSpawnCoordinateY, 0);
+
+            powerup.SetActive(true);
+        }
+
+        powerupSpawnCoordinateY -= 6;
+    }
 }
