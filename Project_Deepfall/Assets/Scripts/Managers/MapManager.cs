e75f6bbb2c29feb90012a6dba28cc0a049230367
MrGri00 <marcgarciat2000@gmail.com>
2022-02-04 09:08:44 +0100

    Cleaned pooling list

--- Project_Deepfall/Assets/Scripts/Managers/MapManager.cs
+++ Project_Deepfall/Assets/Scripts/Managers/MapManager.cs
@@ -16,6 +16,10 @@ public class MapManager : MonoBehaviour
 
     private int rand = 0;
 
+    private GameObject mapSectionBkg = null;
+    private GameObject mapSectionDest = null;
+    private GameObject mapSectionSol = null;
+
     private void Start()
     {
         for (int i = 0; i < 3; i++)
@@ -40,44 +44,36 @@ public class MapManager : MonoBehaviour
 
     void PrintMapSection(int sectionNum)
     {
-        GameObject mapSectionBkg = null;
-        GameObject mapSectionDest = null;
-        GameObject mapSectionSol = null;
+        mapSectionBkg = PoolingManager.Instance.GetPooledObject("Background");
 
         switch (sectionNum)
         {
             case 1:
-                mapSectionBkg = PoolingManager.Instance.GetPooledObject("Background1");
                 mapSectionDest = PoolingManager.Instance.GetPooledObject("Destructible1");
                 mapSectionSol = PoolingManager.Instance.GetPooledObject("Solid1");
                 break;
 
             case 2:
-                mapSectionBkg = PoolingManager.Instance.GetPooledObject("Background2");
                 mapSectionDest = PoolingManager.Instance.GetPooledObject("Destructible2");
                 mapSectionSol = PoolingManager.Instance.GetPooledObject("Solid2");
                 break;
 
             case 3:
-                mapSectionBkg = PoolingManager.Instance.GetPooledObject("Background3");
                 mapSectionDest = PoolingManager.Instance.GetPooledObject("Destructible3");
                 mapSectionSol = PoolingManager.Instance.GetPooledObject("Solid3");
                 break;
 
             case 4:
-                mapSectionBkg = PoolingManager.Instance.GetPooledObject("Background4");
                 mapSectionDest = PoolingManager.Instance.GetPooledObject("Destructible4");
                 mapSectionSol = PoolingManager.Instance.GetPooledObject("Solid4");
                 break;
 
             case 5:
-                mapSectionBkg = PoolingManager.Instance.GetPooledObject("Background5");
                 mapSectionDest = PoolingManager.Instance.GetPooledObject("Destructible5");
                 mapSectionSol = PoolingManager.Instance.GetPooledObject("Solid5");
                 break;
 
             case 6:
-                mapSectionBkg = PoolingManager.Instance.GetPooledObject("Background6");
                 mapSectionDest = PoolingManager.Instance.GetPooledObject("Destructible6");
                 mapSectionSol = PoolingManager.Instance.GetPooledObject("Solid6");
                 break;
@@ -89,12 +85,18 @@ public class MapManager : MonoBehaviour
             mapSectionBkg.transform.SetParent(grid);
             mapSectionBkg.SetActive(true);
             //mapSectionBkg.GetComponent<TilemapController>().ResetMap();
+        }
 
+        if (mapSectionDest != null)
+        {
             mapSectionDest.transform.position = new Vector3(0, mapSpawnCoordinateY, 0);
             mapSectionDest.transform.SetParent(grid);
             mapSectionDest.SetActive(true);
             //mapSectionDest.GetComponent<TilemapController>().ResetMap();
+        }
 
+        if (mapSectionSol != null)
+        {
             mapSectionSol.transform.position = new Vector3(0, mapSpawnCoordinateY, 0);
             mapSectionSol.transform.SetParent(grid);
             mapSectionSol.SetActive(true);
